001ea7d609bac6caff61204196b6f67321a89065
alanis.faishal <alanis.faishal@tokopedia.com>
2022-09-19 11:22:37 +0700

    feat: module a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import ModuleA from './ModuleA';
+import ModuleB from './ModuleB';
+
 import './App.css';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/module-a" component={ModuleA} />
+    <Route exact path="/module-b" component={ModuleB} />
   </Switch>
 );
 
---
+++ src/ModuleB/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModuleB = () => {
+  return (
+    <div>ModuleB</div>
+  )
+}
+
+export default ModuleB
